0008809a5d02cb0e3182ac8066a702d2fc9ec621
Blerona Mulladauti <blerona019@gmail.com>
2025-11-09 15:07:46 +0100

    Added screenshots

--- LABS/Lab1/lab1_exams_management/android/app/build.gradle.kts
+++ LABS/Lab1/lab1_exams_management/android/app/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 android {
     namespace = "com.example.lab1_exams_management"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    //ndkVersion = flutter.ndkVersion
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
